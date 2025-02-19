a5de1a14e8feb395437f71f2beec00612c36495e
Your Name <you@example.com>
2025-02-19 16:23:49 +0500

    added

--- main.js
+++ main.js
@@ -15,14 +15,14 @@ function darkenable() {
        function getWeather() {
            let city = document.getElementById("city").value;
            const api_key = `928754d34862d5c9491233f6ec79e1ff`;
-        let open_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+           let open_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
         fetch(open_api)
             .then(function (response) {
                 if (response.ok) {
                     return response.json()
                 } else {
                     throw new Error("City not found");
-
+                    
                 }
             })
             .then(function (data) {
@@ -34,9 +34,9 @@ function darkenable() {
                     "January", "February", "March", "April", "May", "June",
                     "July", "August", "September", "October", "November", "December"
                 ];
-
+                
                 let formattedDate = `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]}`;
-
+                
                 if (data.main.temp <= 20) {
                     weatherInfo.innerHTML = '<h3> Now ' + '</h3>' +
                         '<h1>' + data.main.temp + '°C <i class="fa-solid fa-snowflake"></i> </h1>' +
@@ -49,25 +49,25 @@ function darkenable() {
                         '<p>Weather in ' + data.weather[0].description + '</p>' +
                         '<p> ' + formattedDate + '</p>' +
                         '<p id ="lastpara"> <i class="fa-solid fa-location-dot"></i> ' + data.name + '</p>'
-                } else {
-                    weatherInfo.innerHTML = '<h3> Now ' + '</h3>' +
+                    } else {
+                        weatherInfo.innerHTML = '<h3> Now ' + '</h3>' +
                         '<h1>' + data.main.temp + '°C <i class="fa-solid fa-cloud-sun"></i> </h1>' +
                         '<p>Weather in ' + data.weather[0].description + '</p>' +
                         '<p> ' + formattedDate + '</p>' +
                         '<p id ="lastpara"> <i class="fa-solid fa-location-dot"></i> ' + data.name + '</p>'
-                }
-                  let today_time = document.getElementById("time");
-                  let time_rise =  data.sys.sunrise;
-                  let time_set =  data.sys.sunset;
-                  let date_new = new Date (time_rise *1000);
-                  let date_new2 = new Date (time_set *1000);
-
+                    }
+                    let today_time = document.getElementById("time");
+                    let time_rise =  data.sys.sunrise;
+                    let time_set =  data.sys.sunset;
+                    let date_new = new Date (time_rise *1000);
+                    let date_new2 = new Date (time_set *1000);
+                    
                   today_time.innerHTML = `<h4>SunRise & Sunset</h4>
                   <p><i class="fas fa-sun"></i>  Sun rise ${date_new.toLocaleTimeString()}</p>
                 <p> <i class="fa-solid fa-moon"></i>  Sun Set ${date_new2.toLocaleTimeString()}</p>`
                 let humidity = document.getElementById("humidity");
                 humidity.innerHTML = `
-                 <h4> Humidity</h4>
+                <h4> Humidity</h4>
                  <p><i class="fa-solid fa-droplet"></i> ${data.main.humidity}%</p>`
                  let pressure = document.getElementById("pressure");
                  pressure.innerHTML = `
@@ -78,7 +78,7 @@ function darkenable() {
                  visibility.innerHTML = `
                  <h4> Visibility</h4>
                  <p> <i class="fa-regular fa-eye"></i> ${data.visibility/1000}km</p> `
-
+                 
                  let feels = document.getElementById("feels");
                  feels.innerHTML = `
                  <h4> Feels like </h4>
@@ -87,19 +87,18 @@ function darkenable() {
                 let lon = data.coord.lon;
                 getAirQuality(lat,lon);
                 dailyfetch(lat,lon);
-                hourly(lat,lon);
                 // console.log(`Latitude: ${lat}, Longitude: ${lon}`);
-
-
+                
+                
             })
             .catch(function (error) {
                 let weatherInfo = document.getElementById("weather-details");
                 weatherInfo.innerHTML = '<p>' + error.message + '</p>';
             });
-    }
-
-
-    //  air pollltiion api
+        }
+        
+        
+        //  air pollltiion api
     function getAirQuality(lat, lon) {
         const api_key2 = `928754d34862d5c9491233f6ec79e1ff`;
         let air_api = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key2}`;
@@ -161,7 +160,7 @@ if (response.ok){
 
     for (let i = 0; i < dataofdaily.list.length; i++) {
         let forcast = dataofdaily.list[i];
-        let date = forcast.dt_txt.split(" ")[0];
+        // let date = forcast.dt_txt.split(" ")[0];
         let tempInCels = (forcast.main.temp_max - 273.15).toFixed(2); // Convert Kelvin to Celsius
         let weatherCondition = forcast.weather[0].main;
 
@@ -197,35 +196,10 @@ if (response.ok){
 }
 
 
-//  Hourly fetch 
-function hourly (lat,lon){
-    let time = dt_txt
-    const api_key4 = `928754d34862d5c9491233f6ec79e1ff`;
-    const apihouly = `https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${time}&appid=${api_key4}`
-fetch(apihouly)
-    .then ( function (response){
-       if(response.ok){
-            return response.json()
-        } else{
-            throw new Error ("City not founded ")
-        }
-    })
-    .then (function (dataOfhoulry){
-        console.log(dataOfhoulry);
-        
-    })
-    .catch(function (error){
-        let hourly = document.getElementById('time_fetch');
-        hourly.innerHTML = '<p>' + error.message + '</p>';
-    })
-}
-
-
-
-
 
    document.querySelector('.main').style.display = 'block';
       getWeather();
+
 
 }
 
